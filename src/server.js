edd3cd746abda92426ae2c548abfdc7c8bb33331
Thalles José <thallesjosesv@gmail.com>
2025-12-10 02:21:29 -0300

    Simplify CORS configuration to allow all origins

--- src/server.js
+++ src/server.js
@@ -12,25 +12,7 @@ const app = express();
 
 // Configuração do CORS
 app.use(cors({
-  origin: function(origin, callback) {
-    const allowedOrigins = ['http://localhost:5173', 'http://localhost:3000', 'https://habitflow-six.vercel.app', 'https://habitflow.com.br'];
-    
-    // Permitir deploys de preview da Vercel (qualquer subdomínio .vercel.app)
-    if (origin && origin.endsWith('.vercel.app')) {
-      return callback(null, true);
-    }
-
-    if (process.env.CORS_ORIGIN) {
-      allowedOrigins.push(...process.env.CORS_ORIGIN.split(','));
-    }
-    
-    if (!origin || allowedOrigins.includes(origin) || process.env.CORS_ORIGIN === '*') {
-      callback(null, true);
-    } else {
-      console.log('🚫 Bloqueado pelo CORS:', origin);
-      callback(new Error(`Not allowed by CORS: ${origin}`));
-    }
-  },
+  origin: '*', // Liberado para qualquer origem
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
